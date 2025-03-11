d8a7350b0871489f02d0b009f0a3a1639fc0b4c5
Abel <pintorgarciaabel4142@gmail.com>
2025-03-11 13:36:03 -0600

    Fix user role verification to check user type instead of role

--- backend/src/middlewares/verifyUserRole.js
+++ backend/src/middlewares/verifyUserRole.js
@@ -10,7 +10,7 @@
  */
 module.exports = (roles) => {
     return (req, res, next) => {
-        if (roles.length > 0 && !roles.includes(req?.user?.role)) {
+        if (roles.length > 0 && !roles.includes(req?.user?.type)) {
             return res.status(403).json({ message: "Access denied" });
         }
         next();
