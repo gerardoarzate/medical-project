0793a3d4fedbfd4d2ecec1f770f9bd17959c623b
Abel <pintorgarciaabel4142@gmail.com>
2025-03-17 18:41:15 -0600

    Add getSpecilitiesAndEmergenciesAssociatedWithMedicData function to retrieve associated specialities for a given medic

--- backend/src/repositories/specialityRepository.js
+++ backend/src/repositories/specialityRepository.js
@@ -11,6 +11,36 @@ const getSpecialities = async () => {
 };
 
 
+/**
+ * 
+ * @param {Number} medicId 
+ * @returns {Promise<SpecialityTypeEmergency[]>} array de especialidades
+ */
+const getSpecilitiesAndEmergenciesAssociatedWithMedicData = async (medicId) => {
+    const queryGetSpecialities = `
+        SELECT etp.*
+        FROM especialidad_tipo_emergencia etp
+        inner JOIN medicos m ON m.id_especialidad = etp.id_especialidad
+        WHERE m.id_usuario = ?
+    `;
+    const [specialities] = await db.query(queryGetSpecialities, [medicId]);
+    return specialities.map(speciality => ({
+        id: speciality.id,
+        specialityId: speciality.id_especialidad,
+        emergencyTypeId: speciality.id_tipo_emergencia
+    }));
+};
+
+
+/**
+ * @typedef {Object} SpecialityTypeEmergency
+ * @property {Number} id
+ * @property {Number} specialityId
+ * @property {Number} emergencyTypeId
+ * 
+ */
+
 module.exports = {
-    getSpecialities
+    getSpecialities,
+    getSpecilitiesAndEmergenciesAssociatedWithMedicData
 };
