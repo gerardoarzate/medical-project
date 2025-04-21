90518ce5f1c6448fe806ae6db3373567d79de18d
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-04-20 21:32:06 -0600

    Create IndexPage

--- frontend/src/routes/IndexPage.jsx
+++ frontend/src/routes/IndexPage.jsx
@@ -1,9 +1,47 @@
+import { BigRoundedButton } from '../components/BigRoundedButton';
+import PatientIcon from '../assets/material-symbols--medical-mask.svg?react';
+import ClinicianIcon from '../assets/maki--doctor.svg?react';
+import { useNavigate } from 'react-router';
+import { BigButton } from '../components/BigButton';
+import Logo from '../assets/logo.svg?react';
 import styles from './IndexPage.module.css';
 
 export const IndexPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <main>
-            <h1>Index Page</h1>
+        <main className={styles.indexPage}>
+            <div className={styles.logoSection}>
+                <div className={styles.logoContainer}>
+                    <Logo/>
+                </div>
+            </div>
+            
+            <div className={styles.contentSection}>
+                <p className={styles.text}>¡Bienvenido! Crea una cuenta para comenzar</p>
+
+                <BigRoundedButton
+                    icon={<PatientIcon />}
+                    onClick={() => navigate('signup-patient')}
+                >
+                    SOY PACIENTE
+                </BigRoundedButton>
+
+                <BigRoundedButton
+                    icon={<ClinicianIcon/>}
+                    onClick={() => navigate('signup-clinician')}
+                >
+                    SOY MÉDICO
+                </BigRoundedButton>
+
+                <p className={styles.text}>Si ya eres usuario</p>
+
+                <BigButton
+                    onClick={() => navigate('login')}
+                >
+                    INICIAR SESIÓN
+                </BigButton>
+            </div>
         </main>
     );
 }
