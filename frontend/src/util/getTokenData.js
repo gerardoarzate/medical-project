e479677f942fbe7e819996d532a64615f8b9f00e
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-04-30 09:55:43 -0600

    Create getTokenData util function

---
+++ frontend/src/util/getTokenData.js
@@ -0,0 +1,13 @@
+const getTokenData = () => {
+    try {
+        const encodedToken = localStorage.getItem('token');
+        const payload = encodedToken.split('.')[1];
+        const decodedPayload = atob(payload);
+        const tokenData = JSON.parse(decodedPayload);
+        return tokenData;
+    } catch(err) {
+        return false;
+    }
+};
+
+export default getTokenData;
