2b652d15becc2cd79c5447387de59d7a7c8c4738
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-04-30 10:32:21 -0600

    Refactor main.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import { IndexPage } from "./routes/IndexPage";
+import { LoginPage } from "./routes/LoginPage";
+import { ClinicianSignUpPage } from "./routes/ClinicianSignUpPage";
+import { PatientSignUpPage } from "./routes/PatientSignUpPage";
+import { Layout } from "./routes/Layout";
+import { AssistancePage } from "./routes/AssistancePage";
+import { CounterpartPage } from './routes/CounterpartPage';
+import { ChatPage } from './routes/ChatPage';
+import { ProfilePage } from './routes/ProfilePage';
+
+export const App = () => {
+    return (
+        <BrowserRouter>
+			<Routes>
+				<Route index element={<IndexPage />} />
+				<Route path="login" element={<LoginPage />} />
+				<Route path="signup-clinician" element={<ClinicianSignUpPage />} />
+				<Route path="signup-patient" element={<PatientSignUpPage />} />
+				<Route path="navigation" element={<Layout />}>
+					<Route index element={<AssistancePage />} />
+					<Route path="assistance" element={<AssistancePage />} />
+					<Route path="counterpart" element={<CounterpartPage />} />
+					<Route path="chat" element={<ChatPage />} />
+					<Route path="profile" element={<ProfilePage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+    );
+};
