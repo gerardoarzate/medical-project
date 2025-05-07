d4b20a82edb4f1014b2ab9ab70465bfe56c6d321
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-06 21:53:18 -0600

    Create assistance page for available clinicians

--- frontend/src/routes/AssistancePage.jsx
+++ frontend/src/routes/AssistancePage.jsx
@@ -1,9 +1,28 @@
 import styles from './AssistancePage.module.css';
+import { RoundedSpecialityHeader } from '../components/RoundedSpecialityHeader';
+import { useToken } from '../contexts/TokenContext';
+import { useProfile } from '../contexts/ProfileContext';
+
+const AvailableClinicianView = ({ profile }) => (
+    <>
+        <RoundedSpecialityHeader speciality={profile?.speciality} name={profile?.name} />
+        <div className={styles.availableClinicianContent}>
+            <div className={styles.clinicianAvailabilityContainer}>
+                <p>Te encuentras</p>
+                <p className={styles.clinicianAvailabilityText}>Disponible</p>
+            </div>
+            <p className={styles.availableClinicianDescription}>Cuando te sea asignada una solicitud verás los detalles aquí</p>
+        </div>
+    </>
+);
 
 export const AssistancePage = () => {
+    const { token } = useToken();
+    const profile = useProfile();
+
     return (
         <main>
-            <h1>Assistance Page</h1>
+            <AvailableClinicianView profile={profile} />
         </main>
     );
 }
