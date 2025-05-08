2c2dbb796c32d25feec8b90b76f210dea51a91f7
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-08 11:40:14 -0600

    Create counterpart page view for available users

--- frontend/src/routes/CounterpartPage.jsx
+++ frontend/src/routes/CounterpartPage.jsx
@@ -1,9 +1,40 @@
 import styles from './CounterpartPage.module.css';
+import { useToken } from '../contexts/TokenContext';
+
+const AvailableClinicianView = () => (
+    <div className={styles.availableClinicianView}>
+        <p className={styles.availableClinicianText}>Cuando te sea asignada una solicitud podrás ver los datos del paciente aquí</p>
+    </div>
+)
+
+const AvailablePatientView = () => (
+    <div className={styles.availablePatientView}>
+        <p className={styles.availablePatientText}>Cuando te sea asignado un médico podrás ver sus datos aquí</p>
+    </div>
+)
+
 
 export const CounterpartPage = () => {
+    const { tokenData } = useToken();
+
+    if (!tokenData) {
+        return;
+    }
+
+    const { type } = tokenData;
+    const isBusy = false;
+
     return (
-        <main>
-            <h1>Counterpart Page</h1>
+        <main className={styles.counterpartPage}>
+            {
+                type == 'MEDICO' ? 
+                    isBusy ? 'busy clinician'
+                    : <AvailableClinicianView />
+                : type == 'PACIENTE' ?
+                    isBusy ? 'busy patient'
+                    : <AvailablePatientView />
+                : null
+            }
         </main>
     );
 }
