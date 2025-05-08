0b90007291454948946c66515fe9379445938cdf
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-08 11:55:42 -0600

    Create chat page view for available users

--- frontend/src/routes/ChatPage.jsx
+++ frontend/src/routes/ChatPage.jsx
@@ -1,9 +1,39 @@
 import styles from './ChatPage.module.css';
+import { useToken } from '../contexts/TokenContext';
+
+const AvailableClinicianView = () => (
+    <div className={styles.availableClinicianView}>
+        <p className={styles.availableClinicianText}>Cuando te sea asignada una solicitud podrás comunicarte con el paciente aquí</p>
+    </div>
+)
+
+const AvailablePatientView = () => (
+    <div className={styles.availablePatientView}>
+        <p className={styles.availablePatientText}>Cuando te sea asignado un médico podrás comunicarte con él aquí</p>
+    </div>
+)
 
 export const ChatPage = () => {
+    const { tokenData } = useToken();
+    
+    if (!tokenData) {
+        return;
+    }
+
+    const { type } = tokenData;
+    const isBusy = false;
+
     return (
-        <main>
-            <h1>Chat Page</h1>
+        <main className={styles.chatPage}>
+            {
+                type == 'MEDICO' ? 
+                    isBusy ? 'busy clinician'
+                    : <AvailableClinicianView />
+                : type == 'PACIENTE' ?
+                    isBusy ? 'busy patient'
+                    : <AvailablePatientView />
+                : null
+            }
         </main>
     );
 }
