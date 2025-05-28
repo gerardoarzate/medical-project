db3f4931d2ba4ed0e11862146dcfafc9d128cacb
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-28 03:10:32 -0600

    Create chat page for busy clinicians and patients

--- frontend/src/routes/ChatPage.jsx
+++ frontend/src/routes/ChatPage.jsx
@@ -1,5 +1,8 @@
 import styles from './ChatPage.module.css';
 import { useToken } from '../contexts/TokenContext';
+import { useAssistanceService } from '../contexts/AssistanceServiceContext';
+import { CounterpartHeader } from '../components/CounterpartHeader';
+import { Chat } from '../components/Chat';
 
 const AvailableClinicianView = () => (
     <div className={styles.availableClinicianView}>
@@ -7,30 +10,53 @@ const AvailableClinicianView = () => (
     </div>
 )
 
+const BusyClinicianView = () => {
+    const { counterpart } = useAssistanceService();
+
+    return (
+        <div className={styles.busyClinicianView}>
+            <CounterpartHeader role='Paciente' name={counterpart.fullName} />
+            <Chat />
+        </div>
+    );
+}
+
 const AvailablePatientView = () => (
     <div className={styles.availablePatientView}>
         <p className={styles.availablePatientText}>Cuando te sea asignado un médico podrás comunicarte con él aquí</p>
     </div>
 )
 
+const BusyPatientView = () => {
+    const { counterpart } = useAssistanceService();
+
+    return (
+        <div className={styles.busyPatientView}>
+            <CounterpartHeader role={counterpart.speciality} name={counterpart.fullName} />
+            <Chat />
+        </div>
+    );
+}
+
 export const ChatPage = () => {
     const { tokenData } = useToken();
+    const { counterpart } = useAssistanceService();
     
     if (!tokenData) {
         return;
     }
 
     const { type } = tokenData;
-    const isBusy = false;
+    const isBusy = counterpart;
 
     return (
         <main className={styles.chatPage}>
             {
                 type == 'MEDICO' ? 
-                    isBusy ? 'busy clinician'
+                    isBusy ? <BusyClinicianView />
                     : <AvailableClinicianView />
                 : type == 'PACIENTE' ?
-                    isBusy ? 'busy patient'
+                    isBusy ? <BusyPatientView />
                     : <AvailablePatientView />
                 : null
             }
