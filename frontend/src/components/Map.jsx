06a23da59e28c0e13482882521b273c725c0d96d
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-28 04:40:02 -0600

    Update Map component to use Leaflet

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,8 +1,20 @@
 import styles from './Map.module.css';
-import placeholder from '../assets/placeholder-map.png';
+import { MapContainer, TileLayer, Circle } from "react-leaflet";
 
 export const Map = () => {
+    const position = [40.7128, -74.006]; // Example: New York City
+    const radiusInMeters = 2;
+
     return (
-        <img className={styles.map} src={placeholder} />
+        <div className={styles.map}>
+            <MapContainer center={position} zoom={16} style={{ height: "100%", width: "100%" }}>
+                <TileLayer
+                    attribution='&copy; OpenStreetMap contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Circle center={position} radius={radiusInMeters} pathOptions={{ color: "red" }} />
+            </MapContainer>
+        </div>
     );
+    
 }
